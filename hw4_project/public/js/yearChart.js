f64abef30140cd063f142b2406c41f1b7c1dbb8f
Настя <nastya-art@yandex.ru>
2017-11-12 00:58:04 +0300

    add yaerChart

--- hw4_project/public/js/yearChart.js
+++ hw4_project/public/js/yearChart.js
@@ -1,74 +1,125 @@
-
 class YearChart {
 
-    /**
-     * Constructor for the Year Chart
-     *
-     * @param electoralVoteChart instance of ElectoralVoteChart
-     * @param tileChart instance of TileChart
-     * @param votePercentageChart instance of Vote Percentage Chart
-     * @param electionInfo instance of ElectionInfo
-     * @param electionWinners data corresponding to the winning parties over mutiple election years
-     */
-    constructor (electoralVoteChart, tileChart, votePercentageChart, electionWinners) {
+  /**
+   * Constructor for the Year Chart
+   *
+   * @param electoralVoteChart instance of ElectoralVoteChart
+   * @param tileChart instance of TileChart
+   * @param votePercentageChart instance of Vote Percentage Chart
+   * @param electionInfo instance of ElectionInfo
+   * @param electionWinners data corresponding to the winning parties over mutiple election years
+   */
+  constructor(electoralVoteChart, tileChart, votePercentageChart, electionWinners) {
 
-        //Creating YearChart instance
-        this.electoralVoteChart = electoralVoteChart;
-        this.tileChart = tileChart;
-        this.votePercentageChart = votePercentageChart;
-        // the data
-        this.electionWinners = electionWinners;
-        
-        // Initializes the svg elements required for this chart
-        this.margin = {top: 10, right: 20, bottom: 30, left: 50};
-        let divyearChart = d3.select("#year-chart").classed("fullView", true);
+    //Creating YearChart instance
+    this.electoralVoteChart = electoralVoteChart;
+    this.tileChart = tileChart;
+    this.votePercentageChart = votePercentageChart;
+    // the data
+    this.electionWinners = electionWinners;
 
-        //fetch the svg bounds
-        this.svgBounds = divyearChart.node().getBoundingClientRect();
-        this.svgWidth = this.svgBounds.width - this.margin.left - this.margin.right;
-        this.svgHeight = 100;
-
-        //add the svg to the div
-        this.svg = divyearChart.append("svg")
-            .attr("width", this.svgWidth)
-            .attr("height", this.svgHeight)
+    // Initializes the svg elements required for this chart
+    this.margin = {
+      top: 10,
+      right: 20,
+      bottom: 30,
+      left: 50
     };
+    let divyearChart = d3.select("#year-chart").classed("fullView", true);
+
+    //fetch the svg bounds
+    this.svgBounds = divyearChart.node().getBoundingClientRect();
+    this.svgWidth = this.svgBounds.width - this.margin.left - this.margin.right;
+    this.svgHeight = 100;
+
+    //add the svg to the div
+    this.svg = divyearChart.append("svg")
+      .attr("width", this.svgWidth)
+      .attr("height", this.svgHeight)
+  };
 
 
-    /**
-     * Returns the class that needs to be assigned to an element.
-     *
-     * @param party an ID for the party that is being referred to.
-     */
-    chooseClass (data) {
-        if (data == "R") {
-            return "yearChart republican";
-        }
-        else if (data == "D") {
-            return "yearChart democrat";
-        }
-        else if (data == "I") {
-            return "yearChart independent";
-        }
+  /**
+   * Returns the class that needs to be assigned to an element.
+   *
+   * @param party an ID for the party that is being referred to.
+   */
+  chooseClass(data) {
+    if (data == "R") {
+      return "yearChart republican";
+    } else if (data == "D") {
+      return "yearChart democrat";
+    } else if (data == "I") {
+      return "yearChart independent";
     }
+  }
 
-    /**
-     * Creates a chart with circles representing each election year, populates text content and other required elements for the Year Chart
-     */
-    update () {
+  /**
+   * Creates a chart with circles representing each election year, populates text content and other required elements for the Year Chart
+   */
+  update() {
 
-        //Domain definition for global color scale
-        let domain = [-60, -50, -40, -30, -20, -10, 0, 10, 20, 30, 40, 50, 60];
 
-        //Color range for global color scale
-        let range = ["#063e78", "#08519c", "#3182bd", "#6baed6", "#9ecae1", "#c6dbef", "#fcbba1", "#fc9272", "#fb6a4a", "#de2d26", "#a50f15", "#860308"];
 
-        //ColorScale be used consistently by all the charts
-        this.colorScale = d3.scaleQuantile()
-            .domain(domain)
-            .range(range);
+    //Domain definition for global color scale
+    let domain = [-60, -50, -40, -30, -20, -10, 0, 10, 20, 30, 40, 50, 60];
 
-       // ******* TODO: PART I *******
+    //Color range for global color scale
+    let range = ["#063e78", "#08519c", "#3182bd", "#6baed6", "#9ecae1", "#c6dbef", "#fcbba1", "#fc9272", "#fb6a4a", "#de2d26", "#a50f15", "#860308"];
+
+    //ColorScale be used consistently by all the charts
+    this.colorScale = d3.scaleQuantile()
+      .domain(domain)
+      .range(range);
+    var self = this;
+    var xScale = d3.scaleBand().range([0, self.svgWidth]);
+
+    xScale.domain(self.electionWinners.map(function(d) {
+      return d.YEAR;
+    }));
+
+    var svg = d3.select("#year-chart").select("svg")
+      .append("g")
+      .attr("class", "year_axis")
+      .attr("transform", "translate(0, 50)")
+      .call(d3.axisBottom(xScale).ticks(self.electionWinners.length)
+        .tickFormat(function(time) {
+          return time;
+        })
+      );
+
+    svg.selectAll("g")
+      .append("circle")
+      .attr("r", 10)
+      .attr("class", function(d) {
+        for (var i = 0; i < self.electionWinners.length; i++) {
+          if (self.electionWinners[i].YEAR == d) {
+            break;
+          }
+        }
+        return  self.chooseClass(self.electionWinners[i].PARTY);
+      })
+      .on("mouseover", function(d) {
+        d3.select(this).classed("highlighted", true);
+      })
+      .on('mouseout', function(d) {
+        d3.selectAll("circle").classed("highlighted", false);
+      })
+      .on("click",function(d) {
+        d3.selectAll("circle").classed("selected", false);
+        d3.select(this).classed("selected", true);
+      });
+
+    svg.selectAll("text")
+      .attr("transform", "translate(0, 10)");
+
+
+    // Compute the minimum and maximum date, and the maximum price.
+
+
+
+
+    // ******* TODO: PART I *******
 
     // Create the chart by adding circle elements representing each election year
     //The circles should be colored based on the winning party for that year
@@ -95,6 +146,6 @@ class YearChart {
     //Call the update method of shiftChart and pass the data corresponding to brush selection.
     //HINT: Use the .brush class to style the brush.
 
-    };
+  };
 
 };
